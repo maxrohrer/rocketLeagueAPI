82914c6e2a1e87d77aab6874c3236eb81357db00
Max Rohrer <maxprohrer@gmail.com>
2019-08-18 08:46:38 -0700

    created jsp calls for cars get routes

--- src/main/java/com/max/rocketLeagueAPI/Controllers/CarController.java
+++ src/main/java/com/max/rocketLeagueAPI/Controllers/CarController.java
@@ -4,10 +4,12 @@ import com.max.rocketLeagueAPI.Models.Car;
 import com.max.rocketLeagueAPI.Repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class CarController {
@@ -15,10 +17,23 @@ public class CarController {
     @Autowired
     CarRepository carRepository;
 
+//    TODO: see if a seperate method should be extracted from get cars route
+    public void filterCarList(Map<String, String> params) {
+
+    }
+
     @GetMapping("/cars")
-    public List<Car> getAllCars() {
-//        TODO: add different calls to factory and logic to make calls based on filters in body
-        return carRepository.findAll();
+    public List<Car> getAllCars(@RequestBody Map<String, String> body) {
+        // return basic findAll if there are no filter params
+        if(body.isEmpty()) return carRepository.findAll();
+        // if platform is chosen return platform exlusive cars
+        if(body.get("platform") != null) return carRepository.findByPlatform(body.get("platform"));
+        // free and rarity filters
+        if(body.get("isFree") != null) {
+            boolean free = Boolean.parseBoolean(body.get("isFree"));
+            if(body.get("rarity") != null) return carRepository.findByRarityAndFree(body.get("rarity"), free);
+            else return carRepository.findByFree(free);
+        }
     }
 
     @GetMapping("/cars/{id}")
--- src/main/java/com/max/rocketLeagueAPI/Repositories/CarRepository.java
+++ src/main/java/com/max/rocketLeagueAPI/Repositories/CarRepository.java
@@ -4,6 +4,13 @@ import com.max.rocketLeagueAPI.Models.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer> {
+    // car filters
+    List<Car> findByPlatform(String platform);
+    List<Car> findByRarity(String rarity);
+    List<Car> findByFree(boolean isFree);
+    List<Car> findByRarityAndFree(String rarity, boolean isFree);
 }
