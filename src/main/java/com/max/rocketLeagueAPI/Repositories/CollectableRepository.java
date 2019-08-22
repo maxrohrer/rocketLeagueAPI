6d528e0605a298de2157fc341f8b539e6ef716c2
Max Rohrer <maxprohrer@gmail.com>
2019-08-22 05:20:38 -0700

    created mapped superclass for collectables

---
+++ src/main/java/com/max/rocketLeagueAPI/Repositories/CollectableRepository.java
@@ -0,0 +1,17 @@
+//TODO: see if it is possible to accept children of collectable in an interface
+
+//package com.max.rocketLeagueAPI.Repositories;
+//
+//import com.max.rocketLeagueAPI.Entities.Collectable;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Repository
+//public interface CollectableRepository extends JpaRepository<Collectable, Integer> {
+//    List<Collectable> findByRarity(Optional<String> rarity);
+//
+//    Collectable findOneByName(String name);
+//}
--- src/main/java/com/max/rocketLeagueAPI/Shared/Generic.java
+++ src/main/java/com/max/rocketLeagueAPI/Shared/Generic.java
@@ -1,13 +1,14 @@
-package com.max.rocketLeagueAPI.Shared;
-
-public class Generic<T> {
-    private T t;
-
-    public T getT() {
-        return t;
-    }
-
-    public void setT(T t) {
-        this.t = t;
-    }
-}
+//TODO: see if it is possible to accept children of collectable in an interface
+//package com.max.rocketLeagueAPI.Shared;
+//
+//public class Generic<T> {
+//    private T t;
+//
+//    public T getT() {
+//        return t;
+//    }
+//
+//    public void setT(T t) {
+//        this.t = t;
+//    }
+//}
