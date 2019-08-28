926e3b0611b8504bed225bc0b99c61e56fc3036c
Max Rohrer <maxprohrer@gmail.com>
2019-08-28 13:03:56 -0400

    set up redirect to swagger docs from root url

--- src/main/java/com/max/rocketLeagueAPI/Controllers/HomeController.java
+++ src/main/java/com/max/rocketLeagueAPI/Controllers/HomeController.java
@@ -1,12 +1,13 @@
 package com.max.rocketLeagueAPI.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
     @RequestMapping("/")
     public String index() {
-        return "TODO: add swagger here";
+        return "redirect:/swagger-ui.html";
     }
 }
